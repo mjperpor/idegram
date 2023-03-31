5f57b7979887493b4f72d7d1d35aa02539875c85
María José <mpe@ccstech.io>
2023-03-31 10:28:03 +0200

    cambios

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+Contrasenya supabase - nunca debería estar puesto aqui
+pNenEpmEnZVkkG72
+
 # Nuxt 3 Minimal Starter
 
 Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,23 @@
 </template>
 <script setup>
 
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://xlcrtmastjguisxahvdj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhsY3J0bWFzdGpndWlzeGFodmRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk1NjA5NDgsImV4cCI6MTk5NTEzNjk0OH0.1D3qe9lcU4aZvV0737sY8gS8knaMk6nMMmefSUQT6BY')
+
+const inputFile = ref();
+
+//async funtion subirFoto() {
+    //const { data, error } = await
+//}
+
+const { data, error } = await supabase
+  .from('photos')
+  .select()
+
+console.log(data)
+
 const { data: users } = await useFetch('https://jsonplaceholder.typicode.com/users')
 
 </script>
